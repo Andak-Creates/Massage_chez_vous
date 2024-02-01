cc48250361930b6861c352dda63a045bb3b2d098
Vindi <kelvinandak@gmail.com>
2024-02-01 01:34:31 +0100

    okay for now

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
     Aos.init({
       duration: 1000, // Animation duration
       easing: "ease-in-out", // Easing function
-      once: true, // Only run once on scroll
+      once: false, // Only run once on scroll
     });
 
     // Disable body overflow when AOS animations are active
